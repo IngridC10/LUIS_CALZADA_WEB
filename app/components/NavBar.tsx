8cf7f5a730babb3d1901073ab337a854655b9b0e
Ingrid <ingridcalzada@MacBook-Pro-de-Ingrid-2.local>
2024-10-05 00:45:26 -0500

    text add

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const NavBar = () => {
-  return <div className="bg-#8C8E8F w-full h-10"></div>;
+  return <div className="bg-#8C8E8F w-full h-10">hola</div>;
 };
 
 export default NavBar;
